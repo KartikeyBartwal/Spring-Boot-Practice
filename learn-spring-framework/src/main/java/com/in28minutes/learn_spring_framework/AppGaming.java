ec799156bdc6277b5a15f08079602ed8e919f825
Kartikey <bartwalkartikey1@gmail.com>
2024-10-30 22:02:07 +0530

    updates

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGaming.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGaming.java
@@ -3,12 +3,14 @@ package com.in28minutes.learn_spring_framework;
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
 import com.in28minutes.learn_spring_framework.game.SuperContra;
+import com.in28minutes.learn_spring_framework.game.PacManGame;
 
 public class AppGaming {
 
 	public static void main(String[] args) {
-		var game = new MarioGame();
+//		var game = new MarioGame();
 //		var game = new SuperContra();
+		var game = new PacManGame();
 		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
