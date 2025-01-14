5e86af00e10b89ee947c74da1a911f1c7942dc42
Kartikey <bartwalkartikey1@gmail.com>
2025-01-14 15:48:03 +0530

    updates

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,30 +3,44 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Service
-@SessionAttributes({"name", "password"})
+@SessionAttributes({ "name", "password" })
 public class TodoService {
 	private static List<Todo> todos = new ArrayList<>();
 
 	public static int todosCount = 0;
-	
+
 	static {
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(2), true));
-		todos.add(new Todo(++todosCount, "in28minutes", "Learn Full Stack Development", LocalDate.now().plusYears(3), false));
+		todos.add(new Todo(++todosCount, "in28minutes", "Learn Full Stack Development", LocalDate.now().plusYears(3),
+				false));
 		todos.add(new Todo(++todosCount, "in28minutes", "Learn Handroid", LocalDate.now().plusYears(4), true));
 	}
 
 	public List<Todo> findByUsername(String username) {
 		return todos;
 	}
-	
+
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
-		todos.add(new Todo(++todosCount, username , description, targetDate, done));
-		return ;
+		todos.add(new Todo(++todosCount, username, description, targetDate, done));
+		return;
+	}
+
+	public void deleteById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+		return;
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
 	}
 }
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todoPage.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todoPage.jsp
@@ -33,6 +33,8 @@
                         <td>${todo.description}</td>
                         <td>${todo.targetDate}</td>
                         <td>${todo.done}</td>
+                    	<td><a href="update-todo?id=${todo.id}" class="btn btn-success"> UPDATE ${todo.id} </a> </td>                        
+                        <td><a href="delete-todo?id=${todo.id}" class="btn btn-warning"> DELETE ${todo.id} </a> </td>
                     </tr>
                 </c:forEach>
             </tbody>
